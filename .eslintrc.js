113dbc4c975845ab23a4ae0d0f7f01bb43cdd5b8
Jeroen Ophof <jeroen@123lens.nl>
2024-08-08 16:34:42 +0200

    Add basic routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    indent: [4, "tab"],
+    "no-tabs": 0,
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,32 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/BrazilView.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HawaiiView.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/JamaicaView.vue"),
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/PanamaView.vue"),
   },
 ];
 
