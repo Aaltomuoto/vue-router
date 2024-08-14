c83d3085235ac5699597c765bba834c4af7becc8
Jeroen Ophof <jeroen@123lens.nl>
2024-08-14 15:53:54 +0200

    Add vue page transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
 <template>
   <TheNavigation />
 
   <div class="container">
-    <router-view :key="$route.path" />
+    <router-view v-slot="{Component}">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 <script>
