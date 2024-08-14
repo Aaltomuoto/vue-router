d8f53b5c7c9ccb17d35c3fe8417d4ad55f91fcc6
Jeroen Ophof <jeroen@123lens.nl>
2024-08-14 16:39:33 +0200

    Expand on page not found route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import sourceData from '@/data.json';
+ 
 const routes = [
   {
     path: "/",
@@ -13,6 +14,18 @@ const routes = [
     component: () =>
       import('@/views/DestinationShow.vue'),
     props: route => ({...route.params, id: parseInt(route.params.id)}),
+    beforeEnter: (to, from ) => {
+      const exists = sourceData.destinations.find(
+        destination => destination.id === parseInt(to.params.id)
+      );
+      if (!exists) return { 
+        name: 'NotFound',
+        // allows keeping the URL while rendering a different page
+        params: { pathMatch: to.path.split('/').slice(1) },
+        query: to.query,
+        hash: to.hash,
+      }
+    },
     children:[
       {
         path: ':experienceSlug',
